ee65a151d731d3cffbde06543aa808ca27639378
Alexis260698 <hena260698@gmail.com>
2023-12-14 11:12:26 -0600

    agregando estilo

--- src/components/InvoiceApp.jsx
+++ src/components/InvoiceApp.jsx
@@ -10,49 +10,60 @@ export const InvoiceApp = () => {
 
     return (
         <>
-            <h1>Ejemplo Factura</h1>
-            <ul>
-                <li>Id: {id}</li>
-                <li>Name: {name}</li>
-            </ul>
 
-            <h3>Datos del cliente</h3>
-            <ul>
-                <li>{nameClient} {lastname}</li>
-                <li> {country}</li>
-                <li>{city}</li>
-                <li>{street} {number}</li>
-            </ul>
+            <div className="container">
+                <div className="card my-3">
+                    <div className="card-header">
+                        Ejemplo Factura
+                    </div>
 
-            <h3>Datos de la empresa</h3>
-            <ul>
-                <li>{company.name}</li>
-                <li> {company.fiscalNumber}</li>
-            </ul>
-
-            <h4>Productos de la factura</h4>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Producto</th>
-                        <th>Precio</th>
-                        <th>Cantidad</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {items.map(({id,product, prize, quantity}) => {
-                        return (
-                            <tr key={id}>
-                                <td>{product}</td>
-                                <td>{prize}</td>
-                                <td>{quantity}</td>
+                    <div className="card-body">
+                    <ul className="list-group">
+                        <li className="list-group-item">Id: {id}</li>
+                        <li className="list-group-item">Name: {name}</li>
+                    </ul>
+                    <div className="row my-4">
+                        <div className="col">
+                            <h3>Datos del cliente</h3>
+                            <ul className="list-group">
+                                <li className="list-group-item active">{nameClient} {lastname}</li>
+                                <li className="list-group-item"> {country} / {city}</li>
+                                <li className="list-group-item">{street} {number}</li>
+                            </ul>
+                        </div>
+                        <div className="col">
+                            <h3>Datos de la empresa</h3>
+                            <ul className="list-group">
+                                <li className="list-group-item active">{company.name}</li>
+                                <li className="list-group-item"> {company.fiscalNumber}</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <h4>Productos de la factura</h4>
+                    <table className="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th>Producto</th>
+                                <th>Precio</th>
+                                <th>Cantidad</th>
                             </tr>
-                        )
-                    })}
+                        </thead>
+                        <tbody>
+                            {items.map(({ id, product, prize, quantity }) => {
+                                return (
+                                    <tr key={id}>
+                                        <td>{product}</td>
+                                        <td>{prize}</td>
+                                        <td>{quantity}</td>
+                                    </tr>
+                                )
+                            })}
 
-                </tbody>
-            </table>
-
+                        </tbody>
+                    </table>
+                </div>
+                </div>
+            </div>
         </>
 
 
